3f409c560a04295d4b5e2bd09eeb50003ff6b11e
STENUNGSUND\rebecca.leckborn <rebecca.leckborn@hotmail.com>
2017-10-10 13:19:57 +0200

    when click a timelineblock -> right or wrong year, preparation for methods

--- Nardada/Nardada/Scripts/JS/test.js
+++ Nardada/Nardada/Scripts/JS/test.js
@@ -1,7 +1,7 @@
 ﻿var testList = [2000, 1995, 1980, 1900, 1904]
 
 function heightCalculation() {
-    let height = (100 - (testList.length * 4)) / (testList.length+1);
+    let height = (100 - (testList.length * 4)) / (testList.length + 1);
     return height;
 }
 
@@ -17,16 +17,56 @@ function getSortList() {
     return sortedList;
 }
 
-$(".timelinecontainer").ready(function () {
+//$(".timelinecontainer").ready(function () {
+//    let list = getSortList();
+//    let height = heightCalculation();
+//    for (var i = 0; i < list.length; i++) {
+//        $(".timelinecontainer").append(`<div class=\"timelineblock\" style=\"height: ${height}%\"> <div class=\"timelineline\"></div> </div> <div class=\"yearblock\">${list[i]}</div>`);
+//    }
+//    $(".timelinecontainer").append(`<div class=\"timelineblock\" style=\"height: ${height}%\"> <div class=\"timelineline\"></div> </div> `);
+//})
+
+$(".timelinecontainer").ready(drawTimeline())
+
+$("body").on("click", ".timelineblock", function () {
+    let yearBefore = $(this).prev().text();
+    let yearAfter = $(this).next().text();
+    console.log(yearBefore);
+    console.log(yearAfter);
+    if (1850 >= yearBefore && 1850 <= yearAfter) {
+        console.log('yes det var rätt!');
+        showCorrectCard();
+        addYearToList();
+        drawTimeline();
+    } else {
+        console.log('nej det blev fel');
+        showWrongCard();
+    }
+    showNextCard();
+})
+
+function showCorrectCard() {
+    console.log('Nu visas det korrekta kortet (grönt)');
+}
+
+function showWrongCard() {
+    console.log('Nu visas det felaktiga kortet (rött)');
+}
+
+function addYearToList() {
+    console.log('Nu läggs det rätta kortet till i listan');
+}
+
+function drawTimeline() {
+    $(".timelinecontainer").empty(); //tömmer vyn för timeline innan den målas om
     let list = getSortList();
     let height = heightCalculation();
     for (var i = 0; i < list.length; i++) {
         $(".timelinecontainer").append(`<div class=\"timelineblock\" style=\"height: ${height}%\"> <div class=\"timelineline\"></div> </div> <div class=\"yearblock\">${list[i]}</div>`);
     }
     $(".timelinecontainer").append(`<div class=\"timelineblock\" style=\"height: ${height}%\"> <div class=\"timelineline\"></div> </div> `);
+}
 
-})
-$("body").on("click", ".timelineblock", function () {
-    let test = $(this).prev().text();
-    console.log(test);
-})
+function showNextCard() {
+    console.log('nästa kort visas');
+}
