59aaca1c0eb39a720415283da88ac5601cb86265
STENUNGSUND\rebecca.leckborn <rebecca.leckborn@hotmail.com>
2017-10-10 16:54:42 +0200

    can dynamicaly create timelines for each player

--- Nardada/Nardada/Scripts/JS/db.js
+++ Nardada/Nardada/Scripts/JS/db.js
@@ -1,15 +1,34 @@
-﻿$("#card").ready(function () {
-    $.ajax({
-        url: '/api/getquestions',
-        method: 'GET'
-    })
-        .done(function (response) {
-            console.log("response", response)
-            
-            $(".cardContent").append(`<div id="categoryHeader">${response[0].Name}</div><div id="questionArea">${response[0].Question}</div>`);
+﻿//$("#card").ready(function () {
+//    $.ajax({
+//        url: '/api/getquestions',
+//        method: 'GET'
+//    })
+//        .done(function (response) {
+//            console.log("response", response)
+//            //let resp = response;
+//            //return resp;
+//            $(".cardContent").append(`<div id="categoryHeader">${response[0].Name}</div><div id="questionArea">${response[0].Question}</div>`);
 
-        })
-        .fail(function (xhr, status, error) {
-            console.log(xhr, status, error);
-        })
-})
+//        })
+//        .fail(function (xhr, status, error) {
+//            console.log(xhr, status, error);
+//        })
+//})
+
+//function showInfoInCard() {
+//    $(".cardContent").append(`<div id="categoryHeader">${response[0].Name}</div><div id="questionArea">${response[0].Question}</div>`);
+//}
+
+//function getCardFromDB() {
+//    $.ajax({
+//        url: '/api/getquestions',
+//        method: 'GET'
+//    })
+//        .done(function (response) {
+//            console.log("response", response)
+//            return response;
+//        })
+//        .fail(function (xhr, status, error) {
+//            console.log(xhr, status, error);
+//        })
+//}
--- Nardada/Nardada/Scripts/JS/test.js
+++ Nardada/Nardada/Scripts/JS/test.js
@@ -1,7 +1,40 @@
-﻿var testList = [2000, 1995, 1980, 1900, 1904]
+﻿var cardDeck;
+var testList = [2000, 1995, 1980, 1900, 1904]
+var player1 = [2000, 1995];
+var player2 = [1980, 1900, 1904, 2012, 1977, 1988, 1954];
+var players = [player1, player2];
 
-function heightCalculation() {
-    let height = (100 - (testList.length * 4)) / (testList.length + 1);
+$(document).ready(function () {
+    getCardDeckFromDB();
+    let nr = 0;
+    console.log(players)
+    console.log(nr)
+    players.forEach(function (player) {
+        drawTimeline(player, nr)
+        nr++; 
+    });
+}) //egentligen när spelet laddas in
+
+function getCardDeckFromDB() {
+    $.ajax({
+        url: '/api/getquestions',
+        method: 'GET'
+    })
+        .done(function (response) {
+            console.log("response", response)
+            cardDeck = response;
+            console.log(cardDeck);
+            showNewCard(); //vill inte ha den här, men behöver vänta på cardDeck laddas in...
+        })
+        .fail(function (xhr, status, error) {
+            console.log(xhr, status, error);
+        })
+}
+
+
+
+function heightCalculation(list) {
+    let height = (100 - (list.length * 4)) / (list.length + 1);
     return height;
 }
 
@@ -17,23 +50,14 @@ function getSortList() {
     return sortedList;
 }
 
-//$(".timelinecontainer").ready(function () {
-//    let list = getSortList();
-//    let height = heightCalculation();
-//    for (var i = 0; i < list.length; i++) {
-//        $(".timelinecontainer").append(`<div class=\"timelineblock\" style=\"height: ${height}%\"> <div class=\"timelineline\"></div> </div> <div class=\"yearblock\">${list[i]}</div>`);
-//    }
-//    $(".timelinecontainer").append(`<div class=\"timelineblock\" style=\"height: ${height}%\"> <div class=\"timelineline\"></div> </div> `);
-//})
-
-$(".timelinecontainer").ready(drawTimeline())
+//$(".timelinecontainer").ready(drawTimeline())
 
 $("body").on("click", ".timelineblock", function () {
     let yearBefore = $(this).prev().text();
     let yearAfter = $(this).next().text();
     console.log(yearBefore);
     console.log(yearAfter);
-    if (1850 >= yearBefore && 1850 <= yearAfter) {
+    if (cardDeck[0].Year >= yearBefore && cardDeck[0].Year <= yearAfter) {
         console.log('yes det var rätt!');
         showCorrectCard();
         addYearToList();
@@ -42,31 +66,46 @@ $("body").on("click", ".timelineblock", function () {
         console.log('nej det blev fel');
         showWrongCard();
     }
-    showNextCard();
+    //getCardFromDB()
+    //showNewCard()
 })
 
 function showCorrectCard() {
     console.log('Nu visas det korrekta kortet (grönt)');
+    $('.cardContent').empty();
+    $('.cardContent').addClass('rightGreen');
+    $('.cardContent').append(`
+            <div class="answer">Rätt</div>
+            <div id="aretVar">Året var</div>
+            <div id="correctYear">${cardDeck[0].Year}</div>`);
 }
 
 function showWrongCard() {
     console.log('Nu visas det felaktiga kortet (rött)');
+    $('.cardContent').empty();
+    $('.cardContent').addClass('wrongRed');
+    $('.cardContent').append(`
+            <div class="answer">Fel</div>
+            <div id="aretVar">Året var</div>
+            <div id="correctYear">${cardDeck[0].Year}</div>`);
 }
 
 function addYearToList() {
     console.log('Nu läggs det rätta kortet till i listan');
 }
 
-function drawTimeline() {
-    $(".timelinecontainer").empty(); //tömmer vyn för timeline innan den målas om
-    let list = getSortList();
-    let height = heightCalculation();
-    for (var i = 0; i < list.length; i++) {
-        $(".timelinecontainer").append(`<div class=\"timelineblock\" style=\"height: ${height}%\"> <div class=\"timelineline\"></div> </div> <div class=\"yearblock\">${list[i]}</div>`);
+function drawTimeline(player, nr) {
+    $(`#player${nr}`).empty(); //tömmer vyn för timeline innan den målas om
+    let sortedList = player.sort();
+    let height = heightCalculation(player);
+    for (var i = 0; i < player.length; i++) {
+        $(`#player${nr}`).append(`<div class=\"timelineblock\" style=\"height: ${height}%\"> <div class=\"timelineline\"></div> </div> <div class=\"yearblock\">${sortedList[i]}</div>`);
     }
-    $(".timelinecontainer").append(`<div class=\"timelineblock\" style=\"height: ${height}%\"> <div class=\"timelineline\"></div> </div> `);
+    $(`#player${nr}`).append(`<div class=\"timelineblock\" style=\"height: ${height}%\"> <div class=\"timelineline\"></div> </div> `);
 }
 
-function showNextCard() {
-    console.log('nästa kort visas');
+function showNewCard() {
+    $(".cardContent").empty();
+    $(".cardContent").append(`<div id="categoryHeader">${cardDeck[0].Name}</div><div id="questionArea">${cardDeck[0].Question}</div>`);
 }
+
