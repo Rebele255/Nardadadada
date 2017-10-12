5f4a876d3f37a93e6566202210b33fa906830778
GOTHENBURG\daniel.rasmusson <d.o.rasmusson@gmail.com>
2017-10-12 21:23:31 +0200

    one game works with flexible amount of players

--- Nardada/Nardada/Scripts/JS/test.js
+++ Nardada/Nardada/Scripts/JS/test.js
@@ -2,10 +2,12 @@
 //skall senare regleras av spelarna i settings vid början av spelet
 var winLimit = 3; 
 //var player1 = [];
-var player1 = { name: "", timeLine: [], color: "" };
+var player1 = { name: "", timeLine: [], color: "", number: 0 };
 //var player2 = [];
-var player2 = { name: "", timeLine: [], color: "" };
-var players = [player1, player2];
+var player2 = { name: "", timeLine: [], color: "", number: 1 };
+var player3 = { name: "Henrik", timeLine: [], color: "yellow", number: 2 };
+
+var players = [player1, player2, player3];
 var currentPlayer = 0;
 
 
@@ -23,17 +25,42 @@ $('#startbutton').click(function () {
     $('#page1').hide();
     $('#page2').show();
 
-    let nr = 0;
-    players.forEach(function (player) {
-        drawTimeline(player, nr)
-        nr++;
-    });
-    $(`#footer1`).css('background-color', `${player1.color}`);
-    $(`#footer2`).css('background-color', `${player2.color}`);
-    $(`#arrow1`).css('border-top', `20px solid ${player1.color}`);
-    $(`#arrow2`).css('border-top', `20px solid ${player2.color}`);
-    $(`#footer1`).text(player1.name);
-    $(`#footer2`).text(player2.name);
+    //let nr = 0;
+    //players.forEach(function (player) {
+    //    drawTimeline(player, nr)
+    //    nr++;
+    //});
+    //$(`#footer1`).css('background-color', `${player1.color}`);
+    //$(`#footer2`).css('background-color', `${player2.color}`);
+    //$(`#arrow1`).css('border-top', `20px solid ${player1.color}`);
+    //$(`#arrow2`).css('border-top', `20px solid ${player2.color}`);
+    //$(`#footer1`).text(player1.name);
+    //$(`#footer2`).text(player2.name);
+    let j = 0;
+
+    for (var i = 0; i < players.length + 1; i++) {
+        if (players.length % 2 == 0) {
+            if (i == players.length/2) {
+                createCardDisplay();
+            }
+            else {
+                createDisplay(players[j]);
+                j++;
+            }
+        }
+        else if (players.length % 2 == 1) {
+            if (i == (players.length-1) /2) {
+                createCardDisplay();
+            }
+            else {
+                createDisplay(players[j]);
+                j++;
+            }
+        }
+    }
+    enableCurrentPlayer();
+
+    showNewCard();
 })
 
 
@@ -46,17 +73,15 @@ $(document).ready(function () {
 })
 
 function prepareNewGame() {
-    currentPlayer = 1;
-
-    changePlayer();
-    //changePlayer();
+    currentPlayer = 0;
 
     //töm timelines för spelarna (annars ligger förra rundans år kvar)
     players.forEach(function (player) {
         player.timeLine = [];
+        getStartYearForPlayers(player);
     });
-    getStartYearForPlayers();
-    enableCurrentPlayer();
+
+    //enableCurrentPlayer();
 }
 
 $('.colordot').click(function () {
@@ -75,14 +100,12 @@ $('.colordot').click(function () {
 })
 
 
-function getStartYearForPlayers() {
-player1.timeLine.push(Math.floor(Math.random() * (2000 - 1900 + 1)) + 1900);
-player2.timeLine.push(Math.floor(Math.random() * (2000 - 1900 + 1)) + 1900);
+function getStartYearForPlayers(player) {
+player.timeLine.push(Math.floor(Math.random() * (2000 - 1900 + 1)) + 1900);
 }
 
 function enableCurrentPlayer() {
-    $('.timelinecontainer').addClass('disablePlayer');
-    $(`#player${currentPlayer}`).removeClass('disablePlayer');
+    $(`#display${currentPlayer}`).addClass('active');
 }
 
 function getCardDeckFromDB() {
@@ -99,18 +122,44 @@ function getCardDeckFromDB() {
             console.log(xhr, status, error);
         })
 }
+function createCardDisplay() {
+    $('#page2').append(`<div class="display">
+            <div class="ful"></div>
+            <div class="card">
+                <div id="logoName">Nardada</div>
+                <div class="cardContent"></div>
+            </div>
+        </div>`)
+}
 
+function createDisplay(player) {
+    let width = displayWidthCalculation();
+    $('#page2').append(`<div class=\"display timeline\" id=\"display${player.number}\" style=\"width: ${width}vw; background-color: ${player.color}\" >
+            <div class=\"timelinecontainer\" id=\"timelinecontainer${player.number}\"/>
+            <div class=\"arrowcontainer\">
+            <div class=\"arrow\" style=\"border-top-color: ${player.color}\"/>
+            </div>
+            <div class=\"todayfooter\">${player.name}</div></div>`) /*{player.name }</div >*/
+    //$(`#arrow${nr}`).css('border-top-color', `${player.color}`);
+    drawTimeline(player, player.number);
+}
 
 //denna funktion skriver ut tidslinjerna
 function drawTimeline(player, nr) {
     //tömmer vyn för timeline innan den målas om
-    $(`#player${nr}`).empty(); 
+    $(`#timelinecontainer${nr}`).empty(); 
+    $(`#timelinecontainer${nr}`).css('background-color', 'white');
     let sortedList = player.timeLine.sort();
     let height = heightCalculation(player.timeLine);
     for (var i = 0; i < player.timeLine.length; i++) {
-        $(`#player${nr}`).append(`<div class=\"timelineblock\" style=\"height: ${height}%\"> <div class=\"timelineline\" style="background-color:${player.color}"></div> </div> <div class=\"yearblock\" >${sortedList[i]}</div>`);
+        $(`#timelinecontainer${nr}`).append(`<div class=\"timelineblock\" style=\"height: ${height}%\"> <div class=\"timelineline\" style="background-color:${player.color}"></div> </div> <div class=\"yearblock\" >${sortedList[i]}</div>`);
     }
-    $(`#player${nr}`).append(`<div class=\"timelineblock\" style=\"height: ${height}%\"> <div class=\"timelineline\" style="background-color:${player.color}"></div> </div> `);
+    $(`#timelinecontainer${nr}`).append(`<div class=\"timelineblock\" style=\"height: ${height}%\"> <div class=\"timelineline\" style="background-color:${player.color}"></div> </div> `);
+}
+
+function displayWidthCalculation() {
+    let width = (100 - 25) / players.length;
+    return width;
 }
 
 function heightCalculation(list) {
@@ -128,7 +177,8 @@ $("body").on("mouseleave", ".timelineblock", function () {
 //nu har tidslinjerna ritats upp, ett kort visats och man ska trycka på tidslinjen för att svara
 $("body").on("click", ".timelineblock", function () {
     
-    $(this).parent().css("pointer-events", "none");
+
+    $(`#display${currentPlayer}`).addClass("lookable");
     //här tar vi in svaret från spelaren
     let yearBefore = $(this).prev().text();
     let yearAfter = $(this).next().text();
@@ -206,12 +256,9 @@ function showNewCard() {
 
 
 function changePlayer() {
+    $(`#display${currentPlayer}`).removeClass('active lookable');
     currentPlayer = (currentPlayer + 1) % players.length;
-
-    $('.timelinecontainer').addClass('disablePlayer');
-    $(`#player${currentPlayer}`).removeClass('disablePlayer');
-    $(`#player${currentPlayer}`).css('pointer-events', 'all');
-
+    $(`#display${currentPlayer}`).addClass('active');
 }
 
 $('#restartbutton').click(function () {
